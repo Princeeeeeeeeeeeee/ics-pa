a9bb4c469f1f25c21b712b4e2cff06f3f7f1b276
tracer-ics2017 <tracer@njuics.org>
2026-06-16 05:06:14 +0800

    >  run 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  05:06:14 up  6:49,  1 user,  load average: 0.32, 0.49, 0.34 63dea40f97adfc4664a8f2945718e4169e232fb

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,10 +7,10 @@ extern uint8_t ramdisk_end;
 #define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 
-intptr_t loader(_Protect *as, const char *filename) {
+uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   if (RAMDISK_SIZE) {
     ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
   }
-  return (intptr_t)DEFAULT_ENTRY;
+  return (uintptr_t)DEFAULT_ENTRY;
 }
