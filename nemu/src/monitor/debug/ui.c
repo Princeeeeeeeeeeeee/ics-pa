6d46b76d73f1d07b38e99ffd1a0cdd9eb3fe3211
tracer-ics2017 <tracer@njuics.org>
2026-06-15 23:01:57 +0800

    >  compile 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:01:57 up 42 min,  1 user,  load average: 0.09, 0.04, 0.06 4456898d29a6d0c5563ebe31f2dca84e780e2b8a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -120,9 +120,9 @@ static int cmd_x(char *args){
   printf("Memory:");
   for(int i=0;i<nLen;i++){
     if(i%4==0)
-      printf("\n0x%x: 0x%02x",addr+i,vaddr_read(addr+i,1));
+      printf("\n0x%x:  0x%02x",addr+i,vaddr_read(addr+i,1));
     else
-      printf("0x%02x",vaddr_read(addr+i,1));
+      printf("  0x%02x",vaddr_read(addr+i,1));
   }
   printf("\n");
   return 0;
