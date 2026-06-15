6288f8cdf2793fdf82334774037820182f6b6575
tracer-ics2017 <tracer@njuics.org>
2026-06-16 01:28:30 +0800

    >  compile 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:28:30 up  3:09,  1 user,  load average: 0.12, 0.08, 0.08 a2e349d183ef320e45d86905820349e06593760a

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -21,7 +21,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       rtl_get_CF(dest);
       break;
     case CC_E:
-      rtl_get_OF(dest);
+      rtl_get_ZF(dest);
       break;
     case CC_BE:
       assert(dest != &t0);
@@ -46,8 +46,10 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       rtl_xor(dest, dest, &t0);
       rtl_get_ZF(&t0);
       rtl_or(dest, dest, &t0);
+    case CC_P: 
+      panic("n86 does not have PF");
+      break;
     default: panic("should not reach here");
-    case CC_P: panic("n86 does not have PF");
   }
 
   if (invert) {
