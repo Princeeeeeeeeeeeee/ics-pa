598ecc4bf1edf22aab2d8653a5d466b1d4432c91
tracer-ics2017 <tracer@njuics.org>
2026-06-16 01:18:22 +0800

    >  compile 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:18:22 up  2:58,  1 user,  load average: 0.21, 0.14, 0.10 83df38683bca3f6851586fe69067cff84ebd29a

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -11,6 +11,17 @@ make_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
+
+#ifdef DEBUG
+  /* 打印用于诊断：subcode, rtl_setcc 返回, 各标志位 */
+  rtlreg_t zf, cf, of;
+  rtl_get_ZF(&zf);
+  rtl_get_CF(&cf);
+  rtl_get_OF(&of);
+  fprintf(stderr, "DBG jcc: subcode=0x%x setcc=%d ZF=%d CF=%d OF=%d jmp_eip=0x%x\n",
+          subcode, (int)t2, (int)zf, (int)cf, (int)of, decoding.jmp_eip);
+#endif
+
   decoding.is_jmp = t2;
 
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
