ed6a253e815d749a388dacbfb05cd8d143f6925d
tracer-ics2017 <tracer@njuics.org>
2026-06-15 22:22:32 +0800

    >  compile 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:22:32 up 3 min,  1 user,  load average: 0.27, 0.22, 0.09 5e9217ceb6667f20de913222c4bb2a0b12f8368e

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -49,8 +49,8 @@ static inline make_DopHelper(SI) {
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
-  fprintf(stderr, "decode SI: eip_of_imm=0x%x width=%d imm=0x%x simm=0x%x opcode=0x%x\n",
-    *eip - op->width, op->width, imm, (uint32_t)op->simm, decoding.opcode);
+  //fprintf(stderr, "decode SI: eip_of_imm=0x%x width=%d imm=0x%x simm=0x%x opcode=0x%x\n",
+  //  *eip - op->width, op->width, imm, (uint32_t)op->simm, decoding.opcode);
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
 #endif
 }
