8d118de8c0c8f705e88e493a62c1e98a96053e8b
tracer-ics2017 <tracer@njuics.org>
2026-06-16 01:31:26 +0800

    >  run 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:31:26 up  3:12,  1 user,  load average: 0.28, 0.17, 0.11 4f73041cb700091595cc5de0477e31e712d37910

--- nexus-am/tests/cputest/tests/sum.c
+++ nexus-am/tests/cputest/tests/sum.c
@@ -3,12 +3,12 @@
 int main() {
 	int i = 1;
 	volatile int sum = 0;
-	while(i <= 4) {
+	while(i <= 100) {
 		sum += i;
 		i ++;
 	}
 
-	nemu_assert(sum == 10);
+	nemu_assert(sum == 5050);
 
 	return 0;
 }
