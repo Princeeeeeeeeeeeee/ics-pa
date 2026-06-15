24ab6b369b8a70bf54c9acf48c2f1812db31ee19
tracer-ics2017 <tracer@njuics.org>
2026-06-15 22:32:31 +0800

    >  compile 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:32:31 up 13 min,  1 user,  load average: 0.19, 0.14, 0.10 4bfe377d16e68a617f2d00f8227359dee03e32

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -39,18 +39,18 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
   //TODO();
-  //op->simm = instr_fetch(eip, op->width);
-  uint32_t imm = instr_fetch(eip, op->width);
+  op->simm = instr_fetch(eip, op->width);
+  //uint32_t imm = instr_fetch(eip, op->width);
   if (op->width == 1) {
-    op->simm = (int32_t)(int8_t)imm;
+    op->simm = (int8_t)op->simm;
   } else { /* op->width == 4 */
-    op->simm = (int32_t)imm;
+    op->simm = (int32_t)op->simm;
   }
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
   fprintf(stderr, "decode SI: eip_of_imm=0x%x width=%d imm=0x%x simm=0x%x opcode=0x%x\n",
-    *eip - op->width, op->width, imm, (uint32_t)op->simm, decoding.opcode);
+    *eip - op->width, op->width, op->simm, (uint32_t)op->simm, decoding.opcode);
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
 #endif
 }
