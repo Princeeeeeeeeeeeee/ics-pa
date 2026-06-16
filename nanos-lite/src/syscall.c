7f465f3927ddb88482e167ee8ca7af3f882865ae
tracer-ics2017 <tracer@njuics.org>
2026-06-17 01:29:44 +0800

    >  run 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:29:44 up  5:39,  1 user,  load average: 0.03, 0.10, 0.11 246230fbcaa80fe48adb5dd39785a4c1c557d7

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -29,7 +29,7 @@ _RegSet* do_syscall(_RegSet *r) {
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-
+  printf("syscall id: %d\n", a[0]);
   switch (a[0]) {
     case SYS_none:
       SYSCALL_ARG1(r) = sys_none();
