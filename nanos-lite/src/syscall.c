f04d7843a2f741033e170aaa9be8c0d5746f1f06
tracer-ics2017 <tracer@njuics.org>
2026-06-17 01:37:44 +0800

    >  run 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:37:44 up  5:47,  1 user,  load average: 0.10, 0.15, 0.13 a05cddd4171d44a188c0a0bc42d3085a36ff27d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,6 +12,7 @@ void sys_exit(int a){
 int sys_write(int fd, void* buf, size_t len){
   if(fd == 1 || fd == 2){
     char c;
+    Log("buffer:%s", (char*)buf);//测试
     for(int i=0; i<len; i++){
       memcpy(&c, buf+i, 1);
       _putc(c);
@@ -23,13 +24,17 @@ int sys_write(int fd, void* buf, size_t len){
   return -1;
 }
 
+int sys_brk(int addr){
+  return 0;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
   a[2] = SYSCALL_ARG3(r);
   a[3] = SYSCALL_ARG4(r);
-  printf("syscall id: %d\n", a[0]);
+  //printf("syscall id: %d\n", a[0]);
   switch (a[0]) {
     case SYS_none:
       SYSCALL_ARG1(r) = sys_none();
@@ -39,6 +44,9 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_write:
       SYSCALL_ARG1(r) = sys_write(a[1], (void*)a[2], a[3]);
+      break;
+    case SYS_brk:
+      SYSCALL_ARG1(r) = sys_brk(a[1]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
