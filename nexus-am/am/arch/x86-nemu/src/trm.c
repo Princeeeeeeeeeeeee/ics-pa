84bec03b5529a5d27f4f7cede723ce2250f5a844
tracer-ics2017 <tracer@njuics.org>
2026-06-16 17:33:28 +0800

    >  run 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  17:33:28 up  2:03,  1 user,  load average: 0.19, 0.07, 0.04 dcf08d0721067ca1b006b7d0f850a5d4dfa77d24

--- nexus-am/am/arch/x86-nemu/src/trm.c
+++ nexus-am/am/arch/x86-nemu/src/trm.c
@@ -51,10 +51,14 @@ void _putc(char ch) {
 }
 
 void _halt(int code) {
-  asm volatile(".byte 0xd6" : :"a"(code));
+  /*asm volatile(".byte 0xd6" : :"a"(code));
 
   // should not reach here
-  while (1);
+  while (1);*/
+  (void)code;
+  asm volatile("hlt");
+  for (;;)
+    asm volatile("pause");
 }
 
 void _trm_init() {
