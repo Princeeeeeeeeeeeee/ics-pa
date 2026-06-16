673477fc1b54357309da91bc1343978973841cf4
tracer-ics2017 <tracer@njuics.org>
2026-06-16 23:16:56 +0800

    >  compile 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  23:16:56 up  3:26,  1 user,  load average: 0.13, 0.16, 0.12 befa6a6d8a798136219d8a1664ca399763cd9d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -184,7 +184,7 @@ int findDominantOp(int p, int q){
       level -= 1;
       if(level < 0){
         printf("error: unmatched ')' at %d in findDominantOp\n", curr);
-        return -1;
+        assert(0);
       }
       continue;
     }
