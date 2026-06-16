1aafa576e3869213c45a7ae65840641616db6489
tracer-ics2017 <tracer@njuics.org>
2026-06-16 22:39:37 +0800

    >  run 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:39:37 up  2:49,  1 user,  load average: 0.14, 0.09, 0.02 c16185ebfabf7e1c699accfbd73827ff0b4793d

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -6,6 +6,7 @@
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PGSIZE    4096    // Bytes mapped by a page
 
+
 struct _RegSet {
   //uintptr_t esi, ebx, eax, eip, edx, error_code, eflags, ecx, cs, esp, edi, ebp;
   //int       irq;
@@ -17,10 +18,10 @@ struct _RegSet {
   uintptr_t eflags;
 };
 
-#define SYSCALL_ARG1(r) 0
-#define SYSCALL_ARG2(r) 0
-#define SYSCALL_ARG3(r) 0
-#define SYSCALL_ARG4(r) 0
+#define SYSCALL_ARG1(r) r->eax
+#define SYSCALL_ARG2(r) r->ebx
+#define SYSCALL_ARG3(r) r->ecx
+#define SYSCALL_ARG4(r) r->edx
 
 #ifdef __cplusplus
 extern "C" {
