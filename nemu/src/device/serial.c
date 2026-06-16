c0d3a7d93971d1cf2f32eb377c17003151b3683e
tracer-ics2017 <tracer@njuics.org>
2026-06-17 01:17:04 +0800

    >  compile 2313922 pa Linux LAPTOP-8V5RQPEC 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  01:17:04 up  5:26,  1 user,  load average: 0.19, 0.10, 0.09 89ddd8aa648075cfa2f05e36f33ec81dc99046cf

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -26,10 +26,14 @@ void serial_io_handler(ioaddr_t addr, int len, bool is_write) {
     if (addr == SERIAL_PORT + LSR_OFFSET) {
       serial_port_base[LSR_OFFSET] = 0x20;
     }
+    else {
+      // 读其他寄存器：返回 0（安全）
+      serial_port_base[addr - SERIAL_PORT] = 0;
+    }
   }
 }
 
 void init_serial() {
   serial_port_base = add_pio_map(SERIAL_PORT, 8, serial_io_handler);
-  serial_port_base[LSR_OFFSET] = 0x20; /* the status is always free */
+  //serial_port_base[LSR_OFFSET] = 0x20; /* the status is always free */
 }
